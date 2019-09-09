334302d6e809d591aca9c84072a3917d1643b69b
denis lussier <denisl@oscg-partners.com>
2019-09-09 06:38:50 +0000

    re-org 2nd pass

---
+++ devel/pg/el7Setup.sh
@@ -0,0 +1,16 @@
+
+sudo yum -y update
+
+sudo yum -y groupinstall 'Development Tools'
+
+$YUM="sudo yum -y install"
+
+$YUM bison-devel readline-devel zlib-devel openssl-devel
+$YUM libxml2-devel libxslt-devel wget openjade 
+$YUM pam-devel openldap-devel uuid-devel 
+
+$YUM llvm-devel clang-devel protobuf-c-devel 
+
+$YUM docbook-dtds docbook-style-dsssl docbook-style-xsl
+$YUM perl-ExtUtils-Embed libevent-devel postgresql-devel mkdocs highlight
+
