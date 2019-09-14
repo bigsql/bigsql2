f0a11794d04c551e4c68eb1866cb19a795949730
denis lussier <denisl@oscg-partners.com>
2019-09-13 20:24:56 -0700

    tweaks

--- devel/pg/gitSetup.sh
+++ devel/pg/gitSetup.sh
@@ -2,3 +2,5 @@ git config --global user.email "denisl@oscg-partners.com"
 git config --global user.name  "denis lussier"
 git config credential.helper store
 git config --global credential.helper "cache --timeout 7200"
+git config --global push.default simple
+
