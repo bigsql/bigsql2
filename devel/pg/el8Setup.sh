e91a387d585c554866b5ca650da70e59cd78b4f5
denis lussier <denisl@oscg-partners.com>
2019-09-27 06:23:03 -0700

    cos8 2nd pass

--- devel/pg/el8Setup.sh
+++ devel/pg/el8Setup.sh
@@ -7,11 +7,18 @@ YUM="sudo yum -y install"
 
 $YUM python3
 
-$YUM bison-devel readline-devel zlib-devel openssl-devel
-$YUM libxml2-devel libxslt-devel sqlite-devel wget openjade 
-$YUM pam-devel openldap-devel uuid-devel
+$YUM readline zlib openssl
+$YUM readline-devel zlib-devel openssl-devel
 
-$YUM llvm-toolset chrpath protobuf-c
+$YUM libxml2 libxslt sqlite pam openldap
+$YUM libxml2-devel libxslt-devel sqlite-devel pam-devel openldap-devel
+
+wget http://rpms.remirepo.net/enterprise/8/remi/x86_64/remi-release-8.0-4.el8.remi.noarch.rpm
+sudo rpm -Uvh remi-release*rpm
+sudo yum --enablerepo=remi -y install openjade
+rm remi-release*rpm
+
+$YUM uuid llvm-toolset chrpath protobuf-c
 
 $YUM docbook-dtds docbook-style-xsl highlight
 sudo pip3 install mkdocs
