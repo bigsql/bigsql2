e5db0a63c54edcbffdef886bf39f1c637525b05a
denis lussier <denisl@oscg-partners.com>
2019-10-03 03:56:09 -0700

    make it clean

--- skeleton.sh
+++ skeleton.sh
@@ -1,7 +1,28 @@
 
-source bp.sh
-./apg install pg11; ./apg start pg11 -y -d demo; ./apg status
-./apg install pglogical -d demo; ./apg status
-./apg install plprofiler -d demo; ./apg status
-./apg install timescaledb -d demo; ./apg status
+if [ "$1" == "12" ]; then
+  source bp.sh
+  ./apg install pg12; ./apg start pg12 -y -d demo;
+
+elif [ "$1" == "11" ]; then
+  source bp.sh
+  ./apg install pg11; ./apg start pg11 -y -d demo; ./apg status
+  ./apg install plprofiler-pg11 -d demo; ./apg status
+  ./apg install timescaledb-pg11 -d demo; ./apg status
+  ./apg install pgspock-pg11 -d demo; ./apg status
+
+elif [ "$1" == "10" ]; then
+  source bp.sh
+  ./apg install pg10; ./apg start pg10 -y -d demo; ./apg status
+  ./apg install plprofiler-pg10 -d demo; ./apg status
+  ./apg install timescaledb-pg10 -d demo; ./apg status
+  ./apg install pgspock-pg10 -d demo; ./apg status
+
+else
+  echo "ERROR: '$1' is an invalid postgres version"
+  exit 1
+fi
+
+echo ""
+echo "Goodbye!"
+exit 0
 
