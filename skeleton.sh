477975a5e5564bb4bee20acf051c6223e66f1b50
denis lussier <denisl@oscg-partners.com>
2019-10-03 08:54:03 -0700

    round out tests fix 10, 11 & 12

--- skeleton.sh
+++ skeleton.sh
@@ -6,16 +6,16 @@ if [ "$1" == "12" ]; then
 elif [ "$1" == "11" ]; then
   source bp.sh
   ./apg install pg11; ./apg start pg11 -y -d demo; ./apg status
-  ./apg install plprofiler-pg11 -d demo; ./apg status
   ./apg install timescaledb-pg11 -d demo; ./apg status
   ./apg install pgspock-pg11 -d demo; ./apg status
+  ./apg install plprofiler-pg11 -d demo; ./apg status
 
 elif [ "$1" == "10" ]; then
   source bp.sh
   ./apg install pg10; ./apg start pg10 -y -d demo; ./apg status
-  ./apg install plprofiler-pg10 -d demo; ./apg status
   ./apg install timescaledb-pg10 -d demo; ./apg status
   ./apg install pgspock-pg10 -d demo; ./apg status
+  ./apg install pgtsql-pg10 -d demo; ./apg status
 
 else
   echo "ERROR: '$1' is an invalid postgres version"
