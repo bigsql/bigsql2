616afcd0e6c1cde1602c2e20a5e764b8db10dbb3
denis lussier <denisl@oscg-partners.com>
2019-11-26 06:55:03 -0800

    more cleanups

--- env.sh
+++ env.sh
@@ -8,14 +8,7 @@ hubV=5.0.3
 P12=12.1-3
 P11=11.6-3
 
-anonV=0.5.0-1
-ddlxV=0.15-1
-omniV=2.16-1
-hypoV=1.1.3-1
-timescaleV=1.5.1-1
-logicalV=2.3.0-1
 profV=4.1-1
-tsqlV=3.0-1
 
 HUB="$PWD"
 SRC="$HUB/src"
@@ -26,8 +19,8 @@ pg="postgres"
 
 OS=`uname -s`
 if [[ $OS == "Linux" ]]; then
-    OS=linux64;
-    outDir=l64
+  OS=linux64;
+  outDir=l64
 fi
 
 plat=$OS
